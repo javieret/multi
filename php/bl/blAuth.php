bd857916545b5b9b06593ecfaf260d50250876e0
javieret <javiereskivel@gmail.com>
2013-04-22 22:09:02 -0500

    Indent

--- php/bl/blAuth.php
+++ php/bl/blAuth.php
@@ -1,16 +1,17 @@
 <?php
 	include ("../dal/Auth.php");
 	$get = new Auth();
+	echo $_POST['hdn'];
 	$info = $get ->getCredentials($_POST['user'], $_POST['pass']);
-
 	if($_POST['hdn']=="compra" and $info){
-		header("Location: ../gracias.php");
+		//header("Location: ../gracias.php");
+	}else if($_POST['hdn']=="index"){
+		//header("Location: ../index.php");
 	}else if($_POST['hdn']){
-		header("Location: ../checarLog.php");
+		//header("Location: ../checarLog.php");
 	}else if($info){
-		header("Location: ../home.php");
+		//header("Location: ../home.php");
 	}else{
-		header("Location: ../index.php");
+		//header("Location: ../index.php");
 	}
-	
 ?>
--- php/js/index.js
+++ php/js/index.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 	  content:  "<form action=bl/blAuth.php method=post>"+
 	  				"<input type=text id=user name=user class=span3 placeholder='Username'>"+
 	  				"<input type=password id=pass name=pass class=span3 placeholder='Password'><input id=btn_login type=submit value=Enter class='btn btn-inverse span3'>"+
+	  				"<input type=hidden id=hdn name=hdn value=index>"+
 	  			"</form>"
 	});
 });
